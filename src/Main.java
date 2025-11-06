f41e702bf308e120440f1ae1af0b14fabf384b87
SaikoDiablo <vikkiti37@gmail.com>
2025-11-06 13:41:34 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,22 @@ import java.util.Random;
 
 public class Main{
     public static void main(String[] args){
-        int[] tab = new int[10];
-        Random rnd = new Random();
-         for(int i = 0; i<tab.length;i++){
-            tab[i]=rnd.nextInt(100)+1;
+        int[] tab = new int[10]; // creating and assigning array
+        Random rnd = new Random(); // using random methos to generate random numbers
+         for(int i = 0; i<tab.length;i++){ // using for loop to fill array with numbers
+            tab[i]=rnd.nextInt(100)+1; //assign random numbers from 1 to 100 that why we use +1, because when we just using 100 it exuleds 100 and includes only 99
          }
-         System.out.println(Arrays.toString(tab));
-         
-        int min = tab[0];
+         System.out.println(Arrays.toString(tab)); // printing the random numbers to console
+
+        int min = tab[0]; 
         int max = tab[0];
-        for(int i = 0; i<tab.length;i++){
+        for(int i = 0; i<tab.length;i++){ // using for loop for finding min and max value
             if(tab[i]<min) min = tab[i];
             if(tab[i]>max) max = tab[i];
         }
-        System.out.println("Minimum number is:" + min);
-        System.out.println("Maximum number is:" + max); 
+        System.out.println("Minimum number is:" + min); // printing results 
+        System.out.println("Maximum number is:" + max); // printing results 
+        
 
     }
 }
